70ac426477c41381eaedd6516b9c976324d1e0fa
Robert Bruckbauer <brombertje@gmail.com>
2025-06-02 09:21:18 +0000

    spring-security

--- app/backend/src/main/java/esy/app/BackendConfiguration.java
+++ app/backend/src/main/java/esy/app/BackendConfiguration.java
@@ -4,12 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import esy.json.JsonJpaEntity;
 import esy.json.JsonMapper;
 import lombok.NonNull;
+
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.*;
 import org.springframework.core.type.filter.AssignableTypeFilter;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy.RepositoryDetectionStrategies;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -26,6 +33,7 @@ import static org.springframework.data.rest.core.mapping.RepositoryDetectionStra
         ignoreResourceNotFound = false,
         value = "classpath:database.properties")
 @EnableTransactionManagement
+@EnableWebSecurity
 public class BackendConfiguration {
 
     /**
@@ -62,6 +70,13 @@ public class BackendConfiguration {
                     }
                 })
                 .forEach(configuration::exposeIdsFor);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(@NonNull final HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 
     @Bean
