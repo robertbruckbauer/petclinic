45034958b4e0050fda0ae06939096ce5ee121669
Robert Bruckbauer <brombertje@gmail.com>
2025-07-11 14:32:52 +0200

    @OutputFile (#27)

--- buildSrc/src/main/java/VersionHistoryTask.java
+++ buildSrc/src/main/java/VersionHistoryTask.java
@@ -1,21 +1,34 @@
 import org.gradle.api.DefaultTask;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 
-public class VersionHistoryTask extends DefaultTask {
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+
+public abstract class VersionHistoryTask extends DefaultTask {
+
+    @OutputFile
+    public abstract RegularFileProperty getChangelog();
 
     @TaskAction
     public void task() {
         try (final var git = JGit.open(getProject().getRootDir())) {
-            final var allTag = git.listAllTag();
-            for (int i = 1; i < allTag.size(); i++) {
-                final var fromTag = allTag.get(i - 1);
-                final var toTag = allTag.get(i);
-                final var allLog = git.listAllLog(fromTag.toRef(), toTag.toRef());
-                System.out.printf("# Version %s%n", fromTag.toSemVer());
-                System.out.println();
-                allLog.forEach(log -> System.out.printf("* %s%n", log));
-                System.out.println();
+            try (final var os = new PrintWriter(getChangelog().get().getAsFile())) {
+                final var allTag = git.listAllTag();
+                for (int i = 1; i < allTag.size(); i++) {
+                    final var fromTag = allTag.get(i - 1);
+                    final var toTag = allTag.get(i);
+                    final var allLog = git.listAllLog(fromTag.toRef(), toTag.toRef());
+                    os.printf("# Version %s%n", fromTag.toSemVer());
+                    os.println();
+                    allLog.forEach(log -> os.printf("* %s%n", log));
+                    os.println();
+                }
             }
+        } catch (FileNotFoundException e) {
+            throw new UncheckedIOException(e);
         }
     }
 }
--- buildSrc/src/main/java/VersionReleaseTask.java
+++ buildSrc/src/main/java/VersionReleaseTask.java
@@ -1,15 +1,28 @@
 import org.gradle.api.DefaultTask;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 
-public class VersionReleaseTask extends DefaultTask {
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+
+public abstract class VersionReleaseTask extends DefaultTask {
+
+    @OutputFile
+    public abstract RegularFileProperty getChangelog();
 
     @TaskAction
     public void task() {
         try (final var git = JGit.open(getProject().getRootDir())) {
-            final var toTag = git.releaseTag();
-            final var allLog = git.listAllLog("HEAD", toTag.toRef());
-            allLog.forEach(log -> System.out.printf("* %s%n", log));
-            System.out.println();
+            try (final var os = new PrintWriter(getChangelog().get().getAsFile())) {
+                final var toTag = git.releaseTag();
+                final var allLog = git.listAllLog("HEAD", toTag.toRef());
+                allLog.forEach(log -> os.printf("* %s%n", log));
+                os.println();
+            } catch (FileNotFoundException e) {
+                throw new UncheckedIOException(e);
+            }
         }
     }
 }
