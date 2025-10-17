5a81c8f892a08390d02aba8a48871ab0b18a2ca0
Robert Bruckbauer <bruckbauer@gmx.at>
2025-10-17 12:15:57 +0200

    Zoneless

--- app/client-angular/src/main/angular/app.config.ts
+++ app/client-angular/src/main/angular/app.config.ts
@@ -1,16 +1,16 @@
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
+  provideZonelessChangeDetection,
 } from "@angular/core";
-import { provideRouter } from "@angular/router";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
 
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideZonelessChangeDetection(),
+    provideRouter(routes, withComponentInputBinding()),
   ],
 };
