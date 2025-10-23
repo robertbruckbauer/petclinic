6a0df9f5141c2916b1ae36a30096386ac05a06ed
Robert Bruckbauer <bruckbauer@gmx.at>
2025-10-23 20:19:42 +0200

    EnumLister: Logik für Filter und Editoren

--- app/client-angular/src/main/angular/pages/basis/enum-lister/enum-lister.ts
+++ app/client-angular/src/main/angular/pages/basis/enum-lister/enum-lister.ts
@@ -2,18 +2,20 @@ import {
   Component,
   DestroyRef,
   OnInit,
+  computed,
   inject,
   input,
   signal,
 } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { SpinnerComponent } from "../../../controls/spinner/spinner";
 import { EnumService } from "../../../services/enum.service";
 import { type EnumItem } from "../../../types/enum.type";
 
 @Component({
   selector: "app-enum-lister",
-  imports: [CommonModule, SpinnerComponent],
+  imports: [CommonModule, ReactiveFormsModule, SpinnerComponent],
   templateUrl: "./enum-lister.html",
   styleUrl: "./enum-lister.css",
 })
@@ -24,11 +26,30 @@ export class EnumListerComponent implements OnInit {
   loading = signal(false);
   allItem = signal<EnumItem[]>([]);
 
+  private formBuilder = inject(FormBuilder);
+  filterForm = this.formBuilder.group({
+    criteria: ["", Validators.required],
+  });
+
   ngOnInit() {
+    this.onFilterClicked();
+  }
+
+  onFilterClicked() {
     this.loading.set(true);
     const subscription = this.restApi.loadAllEnum(this.art()).subscribe({
       next: (res) => {
-        this.allItem.set(res.content);
+        this.allItem.set(
+          res.content.filter((e) => {
+            if (this.filterForm.value.criteria) {
+              const criteria = this.filterForm.value.criteria.toLowerCase();
+              if (e.name.toLowerCase().startsWith(criteria)) return true;
+              if (e.text.toLowerCase().startsWith(criteria)) return true;
+              return false;
+            }
+            return true;
+          })
+        );
       },
       complete: () => {
         this.loading.set(false);
@@ -38,4 +59,31 @@ export class EnumListerComponent implements OnInit {
       subscription.unsubscribe();
     });
   }
+
+  newItemCode = signal(0);
+  itemCode = signal(0);
+  onItemClicked(item: EnumItem) {
+    this.itemCode.set(item.code);
+  }
+  onItemRemoveClicked(item: EnumItem) {
+    this.itemCode.set(item.code);
+    console.log(["onItemRemoveClicked", item]);
+  }
+
+  itemEditorCreate = signal(false);
+  onItemEditorCreateClicked() {
+    this.itemEditorCreate.set(true);
+    this.itemEditorUpdate.set(false);
+  }
+
+  itemEditorUpdate = signal(false);
+  onItemEditorUpdateClicked(item: EnumItem) {
+    this.itemCode.set(item.code);
+    this.itemEditorCreate.set(false);
+    this.itemEditorUpdate.set(true);
+  }
+
+  itemEditorDisabled = computed(
+    () => this.itemEditorCreate() || this.itemEditorUpdate()
+  );
 }
