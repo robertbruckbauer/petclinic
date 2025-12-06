d116be2b3b3c7863a8315c25842bd9040dec1944
Robert Bruckbauer <brombertje@gmail.com>
2025-12-06 11:52:20 +0100

    feat: KI-gestützte API-Dokumentation mit GitHub Copilot (#121)

--- lib/backend-data/src/test/java/esy/app/EsyBackendSpecificationTest.java
+++ lib/backend-data/src/test/java/esy/app/EsyBackendSpecificationTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,18 +16,33 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Tag("fast")
 public class EsyBackendSpecificationTest {
 
-    static <T> void assertDocFor(final Class<T> clazz) {
+    static <T> void assertRestApiFor(final Class<T> clazz) {
         final var root = "../".repeat(2);
-        final var path = Paths.get(root, "doc", "rest", "%sRestApi.adoc".formatted(clazz.getSimpleName()));
+        final var path = Paths.get(root, "doc", "restapi", "%sRestApi.adoc".formatted(clazz.getSimpleName()));
         assertTrue(Files.exists(path), path.toString());
     }
 
     @Test
     void restApi() {
-        assertDocFor(Enum.class);
-        assertDocFor(Owner.class);
-        assertDocFor(Pet.class);
-        assertDocFor(Vet.class);
-        assertDocFor(Visit.class);
+        assertRestApiFor(Enum.class);
+        assertRestApiFor(Owner.class);
+        assertRestApiFor(Pet.class);
+        assertRestApiFor(Vet.class);
+        assertRestApiFor(Visit.class);
+    }
+
+    static <T> void assertGraphQLFor(final Class<T> clazz) {
+        final var root = "../".repeat(2);
+        final var path = Paths.get(root, "doc", "graphql", "%sGraphQL.adoc".formatted(clazz.getSimpleName()));
+        assertTrue(Files.exists(path), path.toString());
+    }
+
+    @Test
+    void graphQL() {
+        //assertGraphQLFor(Enum.class);
+        assertGraphQLFor(Owner.class);
+        //assertGraphQLFor(Pet.class);
+        //assertGraphQLFor(Vet.class);
+        assertGraphQLFor(Visit.class);
     }
 }
