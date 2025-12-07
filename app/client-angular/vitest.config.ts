f9703558dafe9fbcc899ec15dfa3106d5a2c5deb
Robert Bruckbauer <bruckbauer@gmx.at>
2025-12-07 14:48:23 +0100

    vitest mit jsdom

--- app/client-angular/vitest.config.ts
+++ app/client-angular/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['src/main/angular/**/*.test.ts'],
-    environment: 'jsdom',
+    environment: 'node',
     globals: true,
     reporters: ['default', 'junit'],
     outputFile: {
