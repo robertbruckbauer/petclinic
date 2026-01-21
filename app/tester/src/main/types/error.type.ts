08de1af0896a7fe576faa5dfd5e75bf960929ce5
Robert Bruckbauer <bruckbauer@gmx.at>
2026-01-21 09:40:41 +0100

    Types, Owner

---
+++ app/tester/src/main/types/error.type.ts
@@ -0,0 +1,7 @@
+export interface ErrorItem {
+  instance: string;
+  status: number;
+  title?: string;
+  detail?: string;
+  properties?: string[];
+}
--- app/tester/src/test/playwright/fixtures.ts
+++ app/tester/src/test/playwright/fixtures.ts
@@ -1,7 +1,9 @@
 import { test as base } from "@playwright/test";
 import { readFileSync } from "node:fs";
 import { join } from "node:path";
+import { DatabaseSync } from "node:sqlite";
 import { TESTER_URL } from "./global-env.js";
+import { CdcEvent } from "../../main/types/cdc.js";
 
 /**
  * Helper function to log entity operations to the tester service
@@ -34,6 +36,7 @@ export const test = base.extend<{
   logEntity: (entityType: string, entityId: string) => Promise<void>;
   runId: string;
   sqliteFile: string;
+  queryAllEvent: (testName: string) => CdcEvent[];
 }>({
   logEntity: async ({}, use, testInfo) => {
     const boundLogEntity = (entityType: string, entityId: string) =>
@@ -52,6 +55,40 @@ export const test = base.extend<{
     const buildDir = join(process.cwd(), "build");
     const sqliteFile = join(buildDir, `${runId}.sqlite`);
     await use(sqliteFile);
+  },
+
+  queryAllEvent: async ({ sqliteFile }, use) => {
+    const query = (testName: string): CdcEvent[] => {
+      const db = new DatabaseSync(sqliteFile, { readOnly: true });
+      try {
+        const logStmt = db.prepare(`
+          SELECT entity_id, entity_type
+          FROM petclinic_log
+          WHERE test_name = ?
+          LIMIT 1
+        `);
+        const log = logStmt.get(testName) as
+          | { entity_id: string; entity_type: string }
+          | undefined;
+        console.log(["queryAllEvent", log]);
+
+        if (log) {
+          const cdcStmt = db.prepare(`
+          SELECT id, timestamp, entity_id, entity_type, operation
+          FROM petclinic_cdc
+          WHERE entity_id = ?
+            AND entity_type = ?
+          ORDER BY id ASC
+        `);
+          return cdcStmt.all(log.entity_id, log.entity_type) as CdcEvent[];
+        } else {
+          return [];
+        }
+      } finally {
+        db.close();
+      }
+    };
+    await use(query);
   },
 });
 
