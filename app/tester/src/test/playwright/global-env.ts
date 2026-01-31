33296eaf91bab863ff36eb66b9bb0199d908b199
Robert Bruckbauer <bruckbauer@gmx.at>
2026-01-31 10:57:55 +0100

    Smoketest

--- app/tester/src/test/playwright/global-env.ts
+++ app/tester/src/test/playwright/global-env.ts
@@ -9,3 +9,11 @@ export const TESTER_URL = process.env.TESTER_URL || "http://localhost:9090";
  * Spring Boot application acting as the backend for frontend applications.
  */
 export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:8080";
+
+/**
+ * Case: Basic owner CRUD operations
+ * - create owner 'John Doe'
+ * - delete owner
+ */
+export const CASE_OWNER_BASIC_CRUD =
+  "should create and delete 'John Doe' via REST API";
