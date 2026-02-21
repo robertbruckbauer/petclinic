7b53ca39af3ddd4dd4846f9138eec7379c8a65e7
Robert Bruckbauer <brombertje@gmail.com>
2026-02-21 13:14:24 +0100

    copilot: BatchMapping für alle Relationen mit GraphQL collection query (#187)

--- lib/backend-data/src/main/java/esy/app/client/PetGraphqlController.java
+++ lib/backend-data/src/main/java/esy/app/client/PetGraphqlController.java
@@ -1,20 +1,26 @@
 package esy.app.client;
 
+import esy.api.client.Owner;
 import esy.api.client.Pet;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
 public class PetGraphqlController {
 
     private final PetRepository petRepository;
+
+    private final OwnerRepository ownerRepository;
 
     @QueryMapping
     @Transactional
@@ -26,6 +32,21 @@ public class PetGraphqlController {
     @Transactional
     public Optional<Pet> petById(@Argument @NonNull final UUID id) {
         return petRepository.findById(id);
+    }
+
+    @BatchMapping
+    @Transactional
+    public Mono<Map<Pet, Owner>> owner(@NonNull final List<Pet> allPet) {
+        final var allOwnerId = allPet
+                .stream()
+                .map(p -> p.getOwner().getId())
+                .collect(Collectors.toSet());
+        final var ownerById = ownerRepository.findAllById(allOwnerId)
+                .stream()
+                .collect(Collectors.toMap(Owner::getId, o -> o));
+        return Mono.just(allPet
+                .stream()
+                .collect(Collectors.toMap(p -> p, p -> ownerById.get(p.getOwner().getId()))));
     }
 
 }
--- lib/backend-data/src/main/java/esy/app/clinic/VisitGraphqlController.java
+++ lib/backend-data/src/main/java/esy/app/clinic/VisitGraphqlController.java
@@ -1,24 +1,35 @@
 package esy.app.clinic;
 
+import esy.api.client.Pet;
 import esy.api.clinic.QVisit;
+import esy.api.clinic.Vet;
 import esy.api.clinic.Visit;
+import esy.app.client.PetRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
 public class VisitGraphqlController {
 
     private final VisitRepository visitRepository;
+
+    private final PetRepository petRepository;
+
+    private final VetRepository vetRepository;
 
     @QueryMapping
     @Transactional
@@ -54,5 +65,39 @@ public class VisitGraphqlController {
         final var allVisit = new ArrayList<Visit>();
         visitRepository.findAll(query, order).forEach(allVisit::add);
         return allVisit;
+    }
+
+    @BatchMapping
+    @Transactional
+    public Mono<Map<Visit, Pet>> pet(@NonNull final List<Visit> allVisit) {
+        final var allPetId = allVisit
+                .stream()
+                .filter(v -> v.getPet() != null)
+                .map(v -> v.getPet().getId())
+                .collect(Collectors.toSet());
+        final var petById = petRepository.findAllById(allPetId)
+                .stream()
+                .collect(Collectors.toMap(Pet::getId, p -> p));
+        return Mono.just(allVisit
+                .stream()
+                .filter(v -> v.getPet() != null)
+                .collect(Collectors.toMap(v -> v, v -> petById.get(v.getPet().getId()))));
+    }
+
+    @BatchMapping
+    @Transactional
+    public Mono<Map<Visit, Vet>> vet(@NonNull final List<Visit> allVisit) {
+        final var allVetId = allVisit
+                .stream()
+                .filter(v -> v.getVet() != null)
+                .map(v -> v.getVet().getId())
+                .collect(Collectors.toSet());
+        final var vetById = vetRepository.findAllById(allVetId)
+                .stream()
+                .collect(Collectors.toMap(Vet::getId, v -> v));
+        return Mono.just(allVisit
+                .stream()
+                .filter(v -> v.getVet() != null)
+                .collect(Collectors.toMap(v -> v, v -> vetById.get(v.getVet().getId()))));
     }
 }
